90fb858b8ee9be10a912f30ef4f02ff88a68ab52
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to implement percentage values calculation in a GridView unbound column for a group
+
+
+<p>In this example, we have an ASPxGrid bound to SqlDataSource. The data source returns columns. We grouped the grid by one of them. Also, the grid contains an additional unbound column 'percent'. It contains the ratio of the 'UnitPrice' column's value to total 'UnitPrice' column's values for this group. If the grid doesn't have any groups, the 'percent' column will contain  the ratio of the 'UnitPrice' column's value to total 'UnitPrice' column's values of the grid.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,42 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub grid_CustomUnboundColumnData(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewColumnDataEventArgs)
+		If e.Column.FieldName = "percent" Then
+			If (grid.GroupCount > 0) Then
+				Dim itemKeyValue As String = e.GetListSourceFieldValue("ProductID").ToString()
+				Dim summaryValue As Object = grid.GetGroupSummaryValue(GetGroupIndex(itemKeyValue), grid.GroupSummary(0))
+				If summaryValue IsNot Nothing Then
+					e.Value = CDec(e.GetListSourceFieldValue("UnitPrice")) / CDec(summaryValue)
+				End If
+			Else
+				e.Value = CDec(e.GetListSourceFieldValue("UnitPrice")) / CDec(grid.GetTotalSummaryValue(grid.TotalSummary(0)))
+			End If
+
+		End If
+	End Sub
+
+	Protected Function GetGroupIndex(ByVal itemKeyValue As String) As Integer
+		Dim itemIndex As Integer = grid.FindVisibleIndexByKeyValue(itemKeyValue)
+		Dim i As Integer = itemIndex - 1
+		Do While i >= 0
+			If grid.IsGroupRow(i) Then
+				Return i
+			End If
+			i += -1
+		Loop
+		Return -1
+	End Function
+
+End Class
